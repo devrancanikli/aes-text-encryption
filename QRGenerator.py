8e6f476eeb0d1ad5e8bd07aaab9f3f882caa4444
alpsakaci <erenalpsakaci@gmail.com>
2019-10-23 21:13:52 +0300

    codebase refactor

--- QRGenerator.py
+++
@@ -1,9 +0,0 @@
-import pyqrcode
-
-
-def create_qr(text, qr_name):
-    text_qr = pyqrcode.create(text)
-    # text_qr.svg('uca-url.svg', scale=8)
-    # text_qr.eps('uca-url.eps', scale=2)
-    text_qr.png(qr_name, scale=6)
-    # print(text_qr.terminal(quiet_zone=1))
--- QRTest.py
+++ QRTest.py
@@ -1,7 +1,7 @@
 from AESCipher import AESCipher
-from QRGenerator import create_qr
 from PIL import Image
 from pyzbar.pyzbar import decode
+import pyqrcode
 
 
 class QRTest:
@@ -16,6 +16,14 @@ class QRTest:
         self.cipher = AESCipher(bytes(self.key, encoding='utf8'))
 
     @staticmethod
+    def create_qr(text, qr_name):
+        text_qr = pyqrcode.create(text)
+        text_qr.png(qr_name, scale=6)
+        # text_qr.svg('uca-url.svg', scale=8)
+        # text_qr.eps('uca-url.eps', scale=2)
+        # print(text_qr.terminal(quiet_zone=1))
+    
+    @staticmethod
     def decode_qr(image_name):
         data = decode(Image.open(image_name))
         text = data[0].data
@@ -24,17 +32,11 @@ class QRTest:
 
     def encrypt(self, plain_text):
         encrypted = self.cipher.encrypt(plain_text)
-        create_qr(encrypted, 'encrypted_'+self.file_name+'.png')
+        self.create_qr(encrypted, 'encrypted_'+self.file_name+'.png')
         return encrypted
 
     def decrypt(self, encrypted_text):
         decrypted = self.cipher.decrypt(encrypted_text)
-        create_qr(decrypted, 'decrypted_'+self.file_name+'.png')
+        self.create_qr(decrypted, 'decrypted_'+self.file_name+'.png')
         return decrypted
-
-
-
-
-
-
 
